6971b76543d47d0cd227201a1146d21bf10274b2
Vsumbaev <vsumbaev@s-terra.com>
2020-10-31 16:18:30 +0300

    update_recorder_latest

--- group_task2.py
+++ group_task2.py
@@ -1,5 +1,5 @@
 class Group:
-
+    """Класс, описывающий атрибуты для метода create_group"""
     def __init__(self, name, header, footer):
         self.name = name
         self.header = header
---
+++ group_task3.py
@@ -0,0 +1,12 @@
+class Group:
+    """Класс, описывающий атрибуты для метода create_contact"""
+    def __init__(self, name, last_name, nick, company, mobile, mail, b_day, b_mounth, b_year):
+        self.name = name,
+        self.last_name = last_name,
+        self.nick = nick,
+        self.company = company,
+        self.mobile = mobile,
+        self.mail = mail,
+        self.b_day = b_day,
+        self.b_mounth = b_mounth,
+        self.b_year = b_year
