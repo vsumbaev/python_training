f203b1954bde99d97b4c4a768376581d432ec5f8
Vsumbaev <vsumbaev@s-terra.com>
2020-11-16 00:12:35 +0300

    Доработаны вспомогательные классы-помощники, оптимизированы операции, в которых осуществляются переходы между страницами

--- fixture/contact_helper.py
+++ fixture/contact_helper.py
@@ -8,7 +8,8 @@ class ContactHelper:
 
     def open_contacts_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not wd.current_url.endswith("/addressbook/") > 0:
+            wd.find_element_by_link_text("home").click()
 
     def fill_forms_contacts(self, contact):
         wd = self.app.wd
