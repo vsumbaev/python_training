123dbf85502b70c2c72083175a4b27b5a1c96f0b
Vsumbaev <vsumbaev@s-terra.com>
2020-12-06 20:48:13 +0300

    Реализована загрузка тестовых данных из файлов

--- generator/contacts.py
+++ generator/contacts.py
@@ -2,9 +2,9 @@ import random
 import string
 from model.contact import Contact
 import os.path
-import json
 import getopt
 import sys
+import jsonpickle
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
@@ -36,4 +36,5 @@ testdata = [Contact(name="", last_name="", address="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as fl:
-    fl.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    fl.write(jsonpickle.encode(testdata))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-from data.contacts import constant as testdata
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_python_training_task3(app, contact):
+def test_python_training_task3(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contacts_list()
     app.contact.create_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()
